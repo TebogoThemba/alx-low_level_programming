fc83fcb683618c634d2e02fefca934b9044754b0
Tebogo_Themba <tebogothemba18@gmail.com>
2023-02-16 21:02:25 +0000

    update

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - A c program that prints a line to the standard error
- * Return 1 (Success)
+ * main - prints "and that pieces of arts is useful" - Dora Korpar, 2015-10-19"
+ * followed by a newline, to standard error.
+ * Return: Always 1.
 */
 int main(void)
 {
-	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
 	return (1);
 }

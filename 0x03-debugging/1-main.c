03f4c94cf2ffb40cd3f01c436fea094ddf4fa20b
Tebogo_Themba <tebogothemba18@gmail.com>
2023-02-22 07:53:35 +0000

    debugging done

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinte loop
+ * Return: 0
+*/
+
+int main(void)
+{
+	int i;
+
+	print("Infinite loop incoming :(\n");
+	i = 0;
+
+	print("Infinite loop avoided! \\o/\n");
+
+	return (0);
+}

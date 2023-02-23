e492f070dd1ad998e2f3da479343f4ac1d7d71ef
Tebogo_Themba <tebogothemba18@gmail.com>
2023-02-23 20:30:44 +0000

    files update

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_number - print 0123456789
+ * owned by Tebogo Themba
+ * Return: void
+*/
+
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}

5c49c9cd71fcd23d3d6e768f69534f97a1eeaf62
Tebogo_Themba <tebogothemba18@gmail.com>
2023-03-07 00:15:03 +0000

    hot fix

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -18,7 +18,7 @@ char *_strstr(char *haystack, char *needle)
 		while (needle[j] && (haystack[i] == needle[0]))
 		{
 			if (haystack[i + j] == needle[j])
-				i++;
+				j++;
 			else
 				break;
 		}
